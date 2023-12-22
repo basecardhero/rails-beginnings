0f8794327ec77795ed80b6611b1267617a4b8ad9
BaseCardHero <ryan@basecardhero.com>
2023-12-21 22:36:54 -0600

    Added database schema for devise User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create!(email: "user1@example.org",
+      password: "password123", password_confirmation: "password123",
+      confirmed_at: DateTime.now)
+  end
+
+  describe "#email" do
+    before(:each) do
+      @user2 = User.new(email: nil,
+        password: "password123", password_confirmation: "password123",
+        confirmed_at: DateTime.now)
+    end
+
+    it "is required" do
+      expect(@user2.valid?).to be(false)
+      expect(@user2.errors[:email]).to eq(["can't be blank"])
+    end
+
+    it "is unqiue" do
+      @user2.email = @user.email
+      @user2.valid?
+      expect(@user2.errors[:email]).to eq(["has already been taken"])
+    end
+
+    it "is case-insensitive" do
+      @user2.email = @user.email.upcase
+      @user2.valid?
+      expect(@user2.errors[:email]).to eq(["has already been taken"])
+    end
+  end
 end
