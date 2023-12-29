ca252ed4194b72245b1807f5dd5dc6d36807f56a
BaseCardHero <ryan@basecardhero.com>
2023-12-29 11:14:51 -0600

    Added tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+        :recoverable, :rememberable, :validatable, :confirmable
 
-  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :username, presence: true, alphanumeric: true,
+        uniqueness: { case_sensitive: false }, length: { minimum: 6, maximum: 16 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,17 +5,16 @@ RSpec.describe User, type: :model do
     @user = User.create!(email: "user1@example.org", username: "user123",
       password: "password123", password_confirmation: "password123",
       confirmed_at: DateTime.now)
+
+    @user2 = User.new(email: "user2@example.org", username: "user456",
+      password: "password123", password_confirmation: "password123",
+      confirmed_at: DateTime.now)
   end
 
-  describe "#email" do
-    before(:each) do
-      @user2 = User.new(email: nil, username: "user123",
-        password: "password123", password_confirmation: "password123",
-        confirmed_at: DateTime.now)
-    end
-
+  describe ".email" do
     it "is required" do
-      expect(@user2.valid?).to be(false)
+      @user2.email = nil
+      @user2.valid?
       expect(@user2.errors[:email]).to eq(["can't be blank"])
     end
 
@@ -32,16 +31,23 @@ RSpec.describe User, type: :model do
     end
   end
 
-  describe "#username" do
-    before(:each) do
-      @user2 = User.new(email: "user2@example.org", username: nil,
-      password: "password123", password_confirmation: "password123",
-      confirmed_at: DateTime.now)
+  describe ".username" do
+    it "is required" do
+      @user2.username = nil
+      @user2.valid?
+      expect(@user2.errors[:username].first).to eq("can't be blank")
     end
 
-    it "is required" do
+    it "must be at least 6 characters" do
+      @user2.username = "aaaaa"
       @user2.valid?
-      expect(@user2.errors[:username]).to eq(["can't be blank"])
+      expect(@user2.errors[:username]).to eq(["is too short (minimum is 6 characters)"])
+    end
+
+    it "cannot exceed 16 characters" do
+      @user2.username = "aaaaaaaaaaaaaaaaa"
+      @user2.valid?
+      expect(@user2.errors[:username]).to eq(["is too long (maximum is 16 characters)"])
     end
 
     it "is unique" do
@@ -54,6 +60,13 @@ RSpec.describe User, type: :model do
       @user2.username = @user.username.upcase
       @user2.valid?
       expect(@user2.errors[:username]).to eq(["has already been taken"])
+    end
+
+    it "must be alphanumeric" do
+      @user2.username = "#use r123!"
+      @user2.valid?
+
+      expect(@user2.errors[:username]).to eq(["may only contain of letters and numbers"])
     end
   end
 end
