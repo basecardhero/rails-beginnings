8c9a8fee3207cd9f240a844dc5c82383bcda6467
BaseCardHero <ryan@basecardhero.com>
2024-11-26 21:42:03 -0600

    Updated test_helper with additional methods

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,5 +11,17 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
+    def sign_in(user)
+      user = users(user) unless user.is_a? User
+      post session_path, params: { email_address: user.email_address, password: "password" }
+    end
+
+    def sign_out
+      delete session_path
+    end
+
+    def parsed_cookies
+      ActionDispatch::Cookies::CookieJar.build(request, cookies.to_hash)
+    end
   end
 end
