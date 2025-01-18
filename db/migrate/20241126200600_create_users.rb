bdc8ca1cf5fd48f8ba735aac05d03e1860875c5c
BaseCardHero <ryan@basecardhero.com>
2025-01-18 15:33:13 -0600

    Added confirmed_at field to users table

--- db/migrate/20241126200600_create_users.rb
+++ db/migrate/20241126200600_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[8.0]
     create_table :users do |t|
       t.string :email_address, null: false
       t.string :password_digest, null: false
-
+      t.datetime :confirmed_at, null: true
       t.timestamps
     end
     add_index :users, :email_address, unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_26_200601) do
   create_table "users", force: :cascade do |t|
     t.string "email_address", null: false
     t.string "password_digest", null: false
+    t.datetime "confirmed_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
