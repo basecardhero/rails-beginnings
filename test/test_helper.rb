77a31b882b4614621b7a63ecae301936d40f7323
BaseCardHero <ryan@basecardhero.com>
2025-01-18 15:36:13 -0600

    Added doc blocks to test helper methods

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,16 +10,25 @@ module ActiveSupport
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
 
-    # Add more helper methods to be used by all tests here...
+    # Signs in a user by posting to the session path with the user's email and password.
+    #
+    # @param user [User, Symbol] the user to sign in, or a symbol representing a user fixture
+    # @return [void]
     def sign_in(user)
       user = users(user) unless user.is_a? User
-      post session_path, params: { new_session_form: { email_address: user.email_address, password: "password" } }
+      post session_path, params: { new_session_form: { email_address: user.email_address, password: "password123" } }
     end
 
+    # Signs out the current user by deleting the session.
+    #
+    # @return [void]
     def sign_out
       delete session_path
     end
 
+    # Parses the cookies from the request.
+    #
+    # @return [ActionDispatch::Cookies::CookieJar] the parsed cookies
     def parsed_cookies
       ActionDispatch::Cookies::CookieJar.build(request, cookies.to_hash)
     end
