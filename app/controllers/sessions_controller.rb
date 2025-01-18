0c32314c2513c31c1e7b9a82fd88589b5d326d62
BaseCardHero <ryan@basecardhero.com>
2025-01-18 17:21:03 -0600

    Removed flash message when logging out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,7 +26,7 @@ class SessionsController < ApplicationController
   def destroy
     terminate_session
     clear_site_data
-    redirect_to new_session_path, notice: "You have been logged out."
+    redirect_to new_session_path
   end
 
   private
