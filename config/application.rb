bf13a00fee06d8372fb358ca37e7937b8772fb95
BaseCardHero <ryan@basecardhero.com>
2025-01-18 15:41:43 -0600

    Changed project name Rails 8 to Rails Beginnings

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Rails8
+module RailsBeginnings
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
