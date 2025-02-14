d72a65c5fb21a8297779d03385c91a2aa50b39ff
BaseCardHero <ryan@basecardhero.com>
2025-02-13 22:01:17 -0600

    Ran annotation for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  confirmed_at    :datetime
+#  email_address   :string           not null
+#  password_digest :string           not null
+#  username        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email_address  (email_address) UNIQUE
+#  index_users_on_username       (username) UNIQUE
+#
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
